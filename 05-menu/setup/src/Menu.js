a398506f298e5f402ec0d7fa03c5392284c484a3
Rishi Verma <36291789+thecoderishi@users.noreply.github.com>
2021-05-02 02:01:17 +0530

    Add files via upload
    Menu

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-const Menu = () => {
-  return <h2>menu component</h2>;
-};
+const Menu = ({ items }) => {
+  return (
+    <section className='section-center'>
+      {items.map((item) => {
+        const { id, title, img, price, desc } = item
+        return (
+          <article className='menu-item' key={id}>
+            <img className='photo' src={img} alt={title} />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>₹{price}</h4>
+              </header>
+              <p className='item-text'>{desc}</p>
+            </div>
+          </article>
+        )
+      })}
+    </section>
+  )
+}
 
-export default Menu;
+export default Menu
